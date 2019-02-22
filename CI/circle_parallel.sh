6e49ac7063aa7eb6a5ec103aee59f08ffa8c8630
William Cheng <wing328hk@gmail.com>
2019-02-22 18:22:00 +0800

    install Composer programmatically

--- CI/circle_parallel.sh
+++ CI/circle_parallel.sh
@@ -20,10 +20,7 @@ elif [ "$NODE_INDEX" = "3" ]; then
   echo "Running node $NODE_INDEX to test php"
   php -v
   # install composer
-  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-  php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-  php composer-setup.php
-  php -r "unlink('composer-setup.php');"
+  wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
   mvn --quiet verify -Psamples.php
 else
   echo "Running node $NODE_INDEX to test 'samples.circleci.jdk7' defined in pom.xml ..."
