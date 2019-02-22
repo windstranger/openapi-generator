9b6ec63d06857c12ee7b095dee74ff64a9511f28
William Cheng <wing328hk@gmail.com>
2019-02-22 18:20:21 +0800

    install composer in circleci

--- CI/circle_parallel.sh
+++ CI/circle_parallel.sh
@@ -16,6 +16,15 @@ elif [ "$NODE_INDEX" = "2" ]; then
   #export GO_POST_PROCESS_FILE="/usr/local/bin/gofmt -w"
   # not formatting the code as different go versions may format the code a bit different
   ./bin/utils/ensure-up-to-date
+elif [ "$NODE_INDEX" = "3" ]; then
+  echo "Running node $NODE_INDEX to test php"
+  php -v
+  # install composer
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  php composer-setup.php
+  php -r "unlink('composer-setup.php');"
+  mvn --quiet verify -Psamples.php
 else
   echo "Running node $NODE_INDEX to test 'samples.circleci.jdk7' defined in pom.xml ..."
   sudo update-java-alternatives -s java-1.7.0-openjdk-amd64
